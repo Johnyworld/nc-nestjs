ac575ba6df1c704101f1d3088157d9d846cdbdb9
johnyworld <johnyworld@naver.com>
2021-01-17 21:46:58 +0900

    Testing delete and create

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,4 +16,44 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be 4', () => {
+    expect(2+2).toEqual(4);
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['text'],
+        year: 2020,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+  
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['text'],
+        year: 2020,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+      // service.deleteOne();
+    })
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999)
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+      }
+    })
+  });
+
 });
